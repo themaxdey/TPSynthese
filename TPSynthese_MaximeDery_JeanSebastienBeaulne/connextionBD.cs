22a247819bd6a03e0c6cda594a76faaa27fe4737
themaxdey <themaxdey@users.noreply.github.com>
2018-12-18 15:54:41 -0500

    pee pee

---
+++ TPSynthese_MaximeDery_JeanSebastienBeaulne/connextionBD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace TPSynthese_MaximeDery_JeanSebastienBeaulne
+{
+    class connextionBD
+    {
+        private OleDbConnection conn = null;
+        private OleDbDataAdapter daEmploye;
+        private OleDbDataAdapter daMagasin;
+        private DataSet ds;
+        private string chemin = AppDomain.CurrentDomain.BaseDirectory + "\\";
+        private string bd = "TablesMagasin.mbd";
+
+        private string reqSQL1 = "";
+        private string reqSQL2 = "";
+
+        int max_ligne = 0;
+
+        public DataTable peuplerListBoxMagasin()
+        {
+            string connString = "Provider=Microsfot.Jet.OLEBD.4.0;Data Source= " + chemin + bd + ";Persist Security Info=False";
+            DataTable maTable = new DataTable();
+
+            conn = new OleDbConnection();
+            ds = new DataSet();
+
+            conn.ConnectionString = connString;
+            daMagasin = new OleDbDataAdapter(reqSQL1, conn);
+
+            conn.Open();
+
+            daMagasin.Fill(ds, "Employes");
+
+            conn.Close();
+
+            max_ligne = ds.Tables["Enployes"].Rows.Count;
+
+            if(max_ligne > 0)
+            {
+                maTable = ds.Tables[];
+            }
+
+
+
+
+            return maTable;
+        }
+    }
+}
