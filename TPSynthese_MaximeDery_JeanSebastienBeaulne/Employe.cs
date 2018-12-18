83115eb2fbe52d69e3082dfc5b9bff540325b252
Jsbeaulne <39136061+Jsbeaulne@users.noreply.github.com>
2018-12-18 16:32:52 -0500

    B

--- TPSynthese_MaximeDery_JeanSebastienBeaulne/Employe.cs
+++ TPSynthese_MaximeDery_JeanSebastienBeaulne/Employe.cs
@@ -13,13 +13,31 @@ namespace TPSynthese_MaximeDery_JeanSebastienBeaulne
         public DateTime dateEmbauche { get; set; }
         public string posteEmploi { get; set; }
         public double salaire { get; set; }
-        public double comission { get; set; }
+        public double commission { get; set; }
         public string noTelephone { get; set; }
-        public int permanence { get; set; }
+        public Boolean permanence { get; set; }
         public int statut { get; set; }
         public DateTime dateNaissance { get; set; }
         public string commentaire { get; set; }
         public string sexe { get; set; }
         public int noMagasin { get; set; }
+
+        public Employe(int noEmploye, string prenom, DateTime dateEmbauche, string posteEmploi, double salaire, double commission, string noTel, Boolean permanence, 
+                        int status, DateTime dateNaissance, string commentaire, string sexe, int noMagasin)
+        {
+            this.noEmploye = noEmploye;
+            this.prenom = prenom;
+            this.dateEmbauche = dateEmbauche;
+            this.posteEmploi = posteEmploi;
+            this.salaire = salaire;
+            this.commission = commission;
+            this.noTelephone = noTel;
+            this.permanence = permanence;
+            this.statut = statut;
+            this.dateNaissance = dateNaissance;
+            this.commentaire = commentaire;
+            this.sexe = sexe;
+            this.noMagasin = noMagasin;
+        }
     }
 }
