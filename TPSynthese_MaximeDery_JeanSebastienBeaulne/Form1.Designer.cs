1e587625b6bc9bfa4d093751524506fd37101136
themaxdey <themaxdey@users.noreply.github.com>
2018-12-18 17:05:36 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- TPSynthese_MaximeDery_JeanSebastienBeaulne/Form1.Designer.cs
+++ TPSynthese_MaximeDery_JeanSebastienBeaulne/Form1.Designer.cs
@@ -49,7 +49,6 @@
             this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.àProposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.labelTitre = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
@@ -246,15 +245,7 @@
             this.labelTitre.Size = new System.Drawing.Size(56, 25);
             this.labelTitre.TabIndex = 2;
             this.labelTitre.Text = "Titre";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(30, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Ouvrir base de donnee";
-            this.button1.UseVisualStyleBackColor = true;
+
             // 
             // Form1
             // 
