0d72272cbce32639c610a52056a557713aabf0f3
themaxdey <themaxdey@users.noreply.github.com>
2018-12-18 15:15:12 -0500

    push

--- TPSynthese_MaximeDery_JeanSebastienBeaulne/Form1.Designer.cs
+++ TPSynthese_MaximeDery_JeanSebastienBeaulne/Form1.Designer.cs
@@ -38,10 +38,10 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(-2, -1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 60);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(807, 454);
+            this.tabControl1.Size = new System.Drawing.Size(845, 527);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -49,9 +49,9 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(799, 428);
+            this.tabPage1.Size = new System.Drawing.Size(837, 501);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Visualisation";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -59,19 +59,19 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(798, 428);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Intéractions";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(869, 599);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "TP Synthèse Maxime Déry - Jean-Sébastien Beaulne";
+            this.Text = "DB Utilizer 2019";
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
